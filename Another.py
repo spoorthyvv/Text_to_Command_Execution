f1c47aa43ef75be8b4ac7af4bdde4b2adf73fb18
Spoorthy <43513220+spoorthyvv@users.noreply.github.com>
2021-04-20 20:00:04 +0530

    Create Another.py

---
+++ Another.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+  print("Speak")
+  audio_text = r.listen(source)
+  print("Thanks.Time over")
+  a = r.recognize_google(audio_text)
+  print(a, file=open('WRITE YOUR TEXT FILE PATH OVER HERE /home/edwin/making/t.txt','w'))
+  try:
+    print("Text : +r.recognize_google(audio_text))
+  except:
+    print("sorry i didnt get that")
